e6dae4f42ce0d85350b5caacc7b9cb33a72ca90b
palmeirape-ATRIBUICOES <palmeira.pe@gmail.com>
2026-02-01 13:37:23 -0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,1 +1,21 @@
-firebase-messaging-sw.js
+importScripts("https://www.gstatic.com/firebasejs/10.12.5/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.12.5/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDsWJ9VVMSIsQNtifkLVnRNnbzU7favF7s",
+  authDomain: "missoes-drive.firebaseapp.com",
+  projectId: "missoes-drive",
+  messagingSenderId: "198254465545",
+  appId: "1:198254465545:web:dce028792e2c2c57161ed8"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  const title = payload?.notification?.title || "Missoes";
+  const options = {
+    body: payload?.notification?.body || "Nova GLOBAL",
+    icon: "/favicon.ico"
+  };
+  self.registration.showNotification(title, options);
+});
